ea458dd1b5a95c2974a6426b0e0cbc25df0ab304
Christopher Adeszko <cjadeszko@gmail.com>
2018-01-01 16:28:04 -0700

    Assignment 21 - Topics and Posts: complete

---
+++ spec/models/sponsored_post_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe SponsoredPost, type: :model do
+  let(:title) {RandomData.random_sentence}
+  let(:body) {RandomData.random_sentence}
+  let(:price) {rand(100)}
+  let(:name) { RandomData.random_sentence }
+  let(:description) { RandomData.random_paragraph }
+  let(:topic) { Topic.create!(name: name, description: description) }
+  let(:sponsored_post) {topic.sponsored_posts.create!(title: title, body: body, price: price)}
+
+  it { is_expected.to belong_to(:topic) }
+
+  describe "attributes" do
+    it "responds to title, body, and price" do
+      expect(sponsored_post).to have_attributes(title: title, body: body, price: price)
+    end
+  end
+
+end
