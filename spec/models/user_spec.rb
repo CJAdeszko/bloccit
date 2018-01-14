dc53dc6438904525d80486650091ab97a87397e0
Christopher Adeszko <cjadeszko@gmail.com>
2018-01-13 18:19:32 -0700

    Tests written. Need logic for moderator authorization

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,24 +29,24 @@ RSpec.describe User, type: :model do
         expect(user).to respond_to(:role)
       end
 
-  # #2
       it "responds to admin?" do
         expect(user).to respond_to(:admin?)
       end
 
-  # #3
       it "responds to member?" do
         expect(user).to respond_to(:member?)
+      end
+
+      it "responds to moderator?" do
+        expect(user).to respond_to(:moderator?)
       end
     end
 
     describe "roles" do
-  # #4
       it "is member by default" do
         expect(user.role).to eql("member")
       end
 
-  # #5
       context "member user" do
         it "returns true for #member?" do
           expect(user.member?).to be_truthy
@@ -57,7 +57,6 @@ RSpec.describe User, type: :model do
         end
       end
 
-  # #6
       context "admin user" do
         before do
           user.admin!
@@ -71,7 +70,20 @@ RSpec.describe User, type: :model do
           expect(user.admin?).to be_truthy
         end
       end
-      
+
+      context "moderator user" do
+        before do
+          user.moderator!
+        end
+
+        it "returns false for #member?" do
+          expect(user.member?).to be_falsey
+        end
+
+        it "returns true for #moderator?" do
+          expect(user.moderator?).to be_truthy
+        end
+      end
     end
 
 
