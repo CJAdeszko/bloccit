e8c5251d92ee7bb46e202ae1a7d10b9d4a4a722f
Christopher Adeszko <cjadeszko@gmail.com>
2018-01-22 18:45:41 -0700

    Initial commit to save assignment progress

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+  def has_posts(user)
+    if user.posts == 0
+      "{user.name} has not submitted any posts yet."
+    end
+  end
+
+  def has_comments(user)
+    if user.comments == 0
+      "{user.name} has not submitted any comments yet."
+    end
+  end
+
 end
